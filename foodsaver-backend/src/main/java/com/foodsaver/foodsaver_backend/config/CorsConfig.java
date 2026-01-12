f5209642c391ca08b4142efd99da4e988d76ae5f
Soundar Rajan N <Ilayarajan252003@gmail.com>
2026-01-12 13:51:33 +0530

    Create CorsConfig.java

---
+++ foodsaver-backend/src/main/java/com/foodsaver/foodsaver_backend/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.foodsaver.foodsaver_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://foodsaver07.netlify.app",
+                                "http://localhost:5500"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
